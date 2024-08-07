fda7c3b263675fa0ebb65866c1c1aeba7e6aac20
vaibhavpatel <dev.vaibhav.patel@gmail.com>
2024-08-07 17:07:40 +0530

    added salseforce routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+const qs = require("qs");
+
 // get the user details
 const UserDetail = async (req, res) => {
   try {
@@ -61,4 +64,92 @@ const RegisterUser = async (req, res) => {
   }
 };
 
-module.exports = { UpdateUserProfile, UserDetail, RegisterUser };
+const GetAccessToken = async (req, res) => {
+  const url = process.env.SALESFORCE_TOKEN_URL;
+  const data = {
+    grant_type: process.env.GRANT_TYPE,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    username: process.env.USER_NAME,
+    password: process.env.PASSWORD,
+  };
+
+  const config = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
+
+  try {
+    const result = await axios.post(url, qs.stringify(data), config);
+    res.status(200).json(result.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const CreateContact = async (req, res) => {
+  try {
+    const salesforceAccessToken = req.headers.authorization.split(" ")[1];
+    const response = axios.post(
+      `${process.env.SALESFORCE_BASEURL}/sobjects/Contact/`,
+      req.body,
+      {
+        headers: {
+          Authorization: `Bearer ${salesforceAccessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+const GetContact = async (req, res) => {
+  try {
+    const salesforceAccessToken = req.headers.authorization.split(" ")[1];
+    const url = `${process.env.SALESFORCE_BASEURL}/query/?q=SELECT+Id,Name,AccountId,Account.Name+from+Contact`;
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${salesforceAccessToken}`,
+      },
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const UpdateContact = async (req, res) => {
+  try {
+    const salesforceAccessToken = req.headers.authorization.split(" ")[1];
+    const contactId = req.params.id;
+
+    const response = await axios.patch(
+      `${process.env.SALESFORCE_BASEURL}/sobjects/Contact/${contactId}`,
+      req.body,
+      {
+        headers: {
+          Authorization: `Bearer ${salesforceAccessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  UpdateUserProfile,
+  UserDetail,
+  RegisterUser,
+  GetAccessToken,
+  CreateContact,
+  GetContact,
+  UpdateContact,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,21 @@ const {
   UserDetail,
   UpdateUserProfile,
   RegisterUser,
+  GetAccessToken,
+  CreateContact,
+  GetContact,
+  UpdateContact,
 } = require("../controllers/authController");
 
 const userRoutes = Router();
 
 userRoutes.post("/register", RegisterUser);
+
+userRoutes.post("/getToken", GetAccessToken);
+userRoutes.post("/create", CreateContact);
+userRoutes.get("/getContact", GetContact);
+userRoutes.put("/update/:contactId", UpdateContact);
+
 userRoutes.get("/:userId", UserDetail);
 userRoutes.post("/:userId", UpdateUserProfile);
 
